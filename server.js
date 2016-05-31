1406c71992ca69cf171b971822eb367001838cea
mattlaguardia <matthewlaguardia@gmail.com>
2016-05-30 22:16:48 -0700

    hard data added  on server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express'),
     app = express();
 
+var mongoose = require('mongoose');
+
 // parse incoming urlencoded form data
 // and populate the req.body object
 var bodyParser = require('body-parser');
@@ -11,21 +13,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 // HARD CODED DATA //
-var profile = [];
-profile.push({
+var profile = {
   _id:1,
   name: 'Matt',
   github_link: "https://github.com/mattlaguardia",
   github_profile_image: "https://github.com/account",
   current_city: "San Francisco",
   pets: [{name: "Pepper", type: "Dog", breed: "Dachshund"},{name:
-  "Chip", type: "Dog", breed: "Beagle"}],
-  movies: [{"ADD"}],
-  tv_shows: [{"ADD"}]
-});
+  "Chip", type: "Dog", breed: "Beagle"}]
+};
+
+var hikingList = {
+  _id: 1,
+  location: "1",
+  description: "1",
+  trails: "1"
+}
 
 /**********
  * ROUTES *
@@ -34,6 +40,7 @@ profile.push({
 // Serve static files from the `/public` directory:
 // i.e. `/images`, `/scripts`, `/styles`
 app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 /*
  * HTML Endpoints
@@ -54,14 +61,21 @@ app.get('/api', function api_index(req, res) {
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
     documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    base_url: "https://secure-dusk-67818.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Some basic information about me!"},
+      {method: "POST", path: "/api/campsites", description: "My Favorite hiking locations"}
     ]
   })
 });
+
+app.get('/api/profile', function (req, res){
+  db.Profile.find({}, function (err, profile){
+    res.json(profile);
+  })
+})
+
 
 /**********
  * SERVER *
